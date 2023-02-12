7e677df02caee9df15e744fc5a2c0a060c2b8614
MikuroXina <ryosukadnak@gmail.com>
2023-02-12 15:55:04 +0900

    Add AudioUnitAdaptor

--- audio_unit_v3_bridge/Sources/audio_unit_v3_bridge/audio_unit_v3_bridge.swift
+++ audio_unit_v3_bridge/Sources/audio_unit_v3_bridge/audio_unit_v3_bridge.swift
@@ -1,3 +1,37 @@
+import AudioToolbox
+import Cocoa
+import CoreAudioKit
+
+enum BridgeError: Error {
+    case todo
+}
+
+class AudioUnitAdaptor: AUViewController {}
+
+#if os(macOS)
+    extension AudioUnitAdaptor: AUAudioUnitFactory {
+        func createAudioUnit(with _: AudioComponentDescription) throws -> AUAudioUnit {
+            throw BridgeError.todo
+        }
+
+        func requestViewController(completionHandler: @escaping (NSViewController?) -> Void) {
+            completionHandler(self)
+        }
+    }
+#endif
+
+#if os(iOS)
+    extension AudioUnitAdaptor: AUAudioUnitFactory {
+        func createAudioUnit(with _: AudioComponentDescription) throws -> AUAudioUnit {
+            throw BridgeError.todo
+        }
+
+        func requestViewController(completionHandler: @escaping (UIViewController?) -> Void) {
+            completionHandler(self)
+        }
+    }
+#endif
+
 @_cdecl("au3b_hello_world")
 public func au3b_hello_world(input: Bool) -> Bool {
     return !input
